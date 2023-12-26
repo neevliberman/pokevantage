37852c1a103a57af2aac8fdcf5ffaff69df84a00
neevliberman <neevliberman@gmail.com>
2023-12-26 13:06:51 -0500

    Add PokeBrowser component and update dependencies

---
+++ src/components/PokeBrowser.jsx
@@ -0,0 +1,16 @@
+// import './Pokemon.jsx';
+import './pokebrowser.css';
+import { Container, Row, Col, Form } from 'react-bootstrap';
+
+export default function PokeBrowser() {
+    return <>
+        <Form>
+            <input type="text" placeholder="Search Pokemon"/>
+        </Form>
+    </>
+
+}
+
+
+
+
